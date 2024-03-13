cf2da055820aecd0ba5d2f1b80386daf6ee8bb4c
zzw <2318266924@qq.com>
2024-03-13 18:10:22 +0800

    feat:添加gen命令生成配置文件

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/cossim/coss-cli/config"
 	"github.com/cossim/coss-cli/pkg/consul"
+	"github.com/cossim/coss-cli/pkg/pgp"
 	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 var App = &cli.App{
@@ -57,25 +62,167 @@ var App = &cli.App{
 				}
 				return nil
 			},
-			//Subcommands: []*cli.Command{
-			//	{
-			//		Name:  "get",
-			//		Usage: "get config",
-			//		Action: func(cCtx *cli.Context) error {
-			//			ssl := cCtx.Bool("ssl")
-			//			host := cCtx.String("host")
-			//			fmt.Println("ssl: ", ssl)
-			//			fmt.Println("host: ", host)
-			//			client := consul.NewConsulClient(host, "", "", "", ssl)
-			//			token, err := client.GetToken()
-			//			if err != nil {
-			//				return err
-			//			}
-			//			fmt.Println(token)
-			//			return nil
-			//		},
-			//	},
-			//},
+		},
+		{
+			Name:  "gen",
+			Usage: "gen coss config",
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:  "direct",
+					Value: true,
+					Usage: "true or false",
+				},
+				&cli.StringFlag{
+					Name:  "path",
+					Value: "./",
+					Usage: "config path",
+				},
+			},
+			Action: func(cCtx *cli.Context) error {
+				direct := cCtx.Bool("direct")
+				outputDir := cCtx.String("path")
+				cacheDir := "./config"
+				if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+					err := os.Mkdir(cacheDir, 0755) // 创建文件夹并设置权限
+					if err != nil {
+						return err
+					}
+				}
+
+				cacheDir = "./config/interface"
+				if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+					err := os.Mkdir(cacheDir, 0755) // 创建文件夹并设置权限
+					if err != nil {
+						return err
+					}
+				}
+
+				cacheDir = "./config/service"
+				if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+					err := os.Mkdir(cacheDir, 0755) // 创建文件夹并设置权限
+					if err != nil {
+						return err
+					}
+				}
+
+				cacheDir = "./config/common"
+				if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+					err := os.Mkdir(cacheDir, 0755) // 创建文件夹并设置权限
+					if err != nil {
+						return err
+					}
+				}
+
+				if direct {
+					// 生成接口配置文件
+					for _, name := range config.InterfaceList {
+						configStr := config.GenInterfaceConfig(name, config.InetrfaceConfig[name], direct)
+						filePath := filepath.Join(outputDir+"/config/interface/", fmt.Sprintf("%s.yaml", config.InetrfaceName[name]))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+
+					// 生成服务配置文件
+					for _, name := range config.ServiceList {
+						configStr := config.GenServiceConfig(name, config.ServiceConfig[name])
+						filePath := filepath.Join(outputDir+"/config/service/", fmt.Sprintf("%s.yaml", config.ServiceName[name]))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+				} else {
+					fmt.Println("生成 consul 配置文件")
+					// 生成接口配置文件
+					for _, name := range config.InterfaceList {
+						configStr := config.GenConsulInterfaceConfig(name, config.InetrfaceConfig[name], direct)
+						filePath := filepath.Join(outputDir+"/config/interface/", fmt.Sprintf("%s.yaml", config.InetrfaceName[name]))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+
+					// 生成服务配置文件
+					for _, name := range config.ServiceList {
+						configStr := config.GenConsulServiceConfig(name, config.ServiceConfig[name])
+						filePath := filepath.Join(outputDir+"/config/service/", fmt.Sprintf("%s.yaml", config.ServiceName[name]))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+
+					//生成公共配置
+					for _, name := range config.ConsulCommonList {
+						configStr := config.GenConsulCommonConfig(name)
+						filePath := filepath.Join(outputDir+"/config/common/", fmt.Sprintf("%s.yaml", name))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+				}
+
+				// 生成公共配置文件
+				for _, name := range config.CommonClist {
+					configStr := config.GenCommonConfig(name)
+					if name == "consul" {
+						filePath := filepath.Join(outputDir+"/config/common/", fmt.Sprintf("%s.json", name))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					} else {
+						filePath := filepath.Join(outputDir+"/config/common/", fmt.Sprintf("%s.yaml", name))
+						err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+						if err != nil {
+							fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+						} else {
+							fmt.Printf("生成 %s 成功\n", filePath)
+						}
+					}
+
+				}
+
+				//生成docker-compose
+				configStr := ""
+				if direct {
+					configStr = config.GenDockerCompose(false)
+				} else {
+					configStr = config.GenDockerCompose(true)
+				}
+				filePath := filepath.Join(outputDir, "docker-compose.yaml")
+				err := ioutil.WriteFile(filePath, []byte(configStr), 0644)
+				if err != nil {
+					fmt.Printf("写入文件 %s 失败：%v\n", filePath, err)
+					return err
+				} else {
+					fmt.Printf("生成 %s 成功\n", filePath)
+				}
+
+				//生成pgp公私钥
+				err = pgp.GenerateKeyPair()
+				if err != nil {
+					return err
+				}
+
+				return nil
+			},
 		},
 	},
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,20 @@ module github.com/cossim/coss-cli
 
 go 1.22.0
 
-require github.com/urfave/cli/v2 v2.27.1
+require (
+	github.com/ProtonMail/gopenpgp/v2 v2.7.5
+	github.com/urfave/cli/v2 v2.27.1
+)
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230717121422-5aa5874ade95 // indirect
+	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
